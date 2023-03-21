590910154627a8de7e1fd32a98e5b960c2286638
ElJosecito <imjose0314@gmail.com>
2023-03-21 01:53:03 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Catalogo from "./assets/components/Catalogo"
+import Login from "./assets/components/Login"
+
+
+function App() {
+
+  return (
+    <div className="App">
+     <h1 className="text-3xl font-bold">
+      Uncanny Store!
+    </h1>
+    
+    </div>
+  )
+}
+
+export default App
