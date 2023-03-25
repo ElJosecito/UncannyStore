2a224850e71b0cb551801b34abc49e812e15a955
ElJosecito <imjose0314@gmail.com>
2023-03-25 00:02:59 -0400

    Fixed margins

--- src/assets/components/Catalogo.jsx
+++ src/assets/components/Catalogo.jsx
@@ -4,10 +4,10 @@ import Items from "./Items";
 function Catalogo({ data }) {
   return (
     <>
-      <section className=" w-full min-h-screen h-fit flex flex-col items-center">
-        <div className="max-w-screen-xl w-full">
+      <section className=" w-full min-h-screen h-fit flex justify-center">
+        <div className="max-w-screen-xl w-full flex flex-col items-center">
         <h2 className="font-mono text-3xl font-semibold self-start mb-10">Popular products from <br/> <span className="font-extrabold text-blue-600">Uncanny</span></h2>
-        <div className="w-fit h-screen grid grid-cols-1 md:grid-cols-2 md:gap-5 lg:grid-cols-3 xl:grid-cols-4 gap-16">
+        <div className="w-fit min-h-screen h-fit grid grid-cols-1 md:grid-cols-2 md:gap-5 lg:grid-cols-3 xl:grid-cols-4 gap-16">
         {data.map((respon) => {
           return (
             <Items
