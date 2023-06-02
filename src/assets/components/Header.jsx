4e60dbda67ff9ee96a5cc110a68401936037bb4a
ElJosecito <imjose0314@gmail.com>
2023-06-02 15:35:53 -0400

    Responsive 1.0

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -5,16 +5,34 @@ function Header() {
     <div className='flex items-center justify-between w-full h-16  pl-10 pr-10 z-50 fixed bg-white'>
         <h2 className='text-2xl font-extrabold'>Uncanny</h2>
         
-        <ul className='w-80 flex items-center justify-between text-base font-bold '>
+        <ul className='w-80 items-center justify-between text-base font-bold hidden lg:flex'>
             <li>Home</li>
             <li>Products</li>
             <li>Categories</li>
         </ul>
 
-        <div className='text-base font-bold'>
+        <div className='text-base font-bold lg:flex hidden'>
             <button className='bg-slate-100 m-1 p-2 pl-5 pr-5 rounded-md'>Login</button>
             <button className='bg-blue-600 text-slate-100 m-1 p-2 pl-5 pr-5 rounded-md'>Register</button>
         </div>
+
+        <div className="flex-none lg:hidden">
+              <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-6 h-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
     </div>
   )
 }
--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,55 +1,65 @@
 import React from "react";
-import women from "../images/women.png"
+import women from "../images/women.png";
 
 function Hero() {
   return (
     <div className="w-full min-h-screen h-fit flex justify-center">
-      <div className="max-w-screen-xl w-full bg-white flex justify-between items-center">
-        <div className="w-[40rem] h-full flex flex-col justify-center items-start">
-            <h1 className="font-mono text-6xl font-semibold">Best Place to buy<br/><span className="font-extrabold text-blue-600">Everything</span></h1>
-            <p className="font-sans font-light mt-5 w-[28rem]">At Uncanny Store you can shop whatever you think at very accesible prices, so... What do you think, Let's buy something!!</p>
+      <div className="max-w-screen-xl w-full bg-white xl:flex-row flex-col flex justify-between items-center">
+        <div className="w-[40rem] h-full flex flex-col justify-center items-start ml-10 mt-28 xl:mt-0">
+          <h1 className="font-mono text-6xl font-semibold">
+            Best Place to buy
+            <br />
+            <span className="font-extrabold text-blue-600">Everything</span>
+          </h1>
+          <p className="font-sans font-light mt-5 w-[28rem]">
+            At Uncanny Store you can shop whatever you think at very accesible
+            prices, so... What do you think, Let's buy something!!
+          </p>
 
-            <button className="bg-blue-600 text-slate-100 p-2 pl-5 pr-5 rounded-md mt-10"> Shopping Now</button>
-
+          <button className="bg-blue-600 text-slate-100 p-2 pl-5 pr-5 rounded-md mt-10">
+            {" "}
+            Shopping Now
+          </button>
         </div>
-      
-        <div className="w-[35rem] relative max-h-screen">
-            <img src={women} alt="" className="w-96 absolute top-10 left-12"/>
-            {/* <div>
+
+        <div className="w-[35rem] xl:relative max-h-screen hidden xl:block">
+          <img src={women} alt="" className="w-96 absolute top-10 left-12" />
+          {/* <div>
                 <button className="bg-blue-600 text-slate-100 m-1 p-2 pl-5 pr-5 rounded-md absolute top-60 left-80">Start Shopping</button>
             </div> */}
-         <div>
-         <svg
-            id="sw-js-blob-svg"
-            viewBox="0 0 100 100"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
-                <stop
-                  id="stop1"
-                  stopColor="rgba(222, 197, 166, 1)"
-                  offset="0%"
-                ></stop>
-                <stop
-                  id="stop2"
-                  stopColor="rgba(222, 197, 166, 1)"
-                  offset="100%"
-                ></stop>
-              </linearGradient>
-            </defs>
-            <path
-              fill="url(#sw-gradient)"
-              d="M22.1,-37.9C29.2,-34.2,35.7,-29.3,39.2,-22.8C42.7,-16.3,43.1,-8.1,42.5,-0.3C41.9,7.5,40.4,14.9,37.2,22C34,29,29.2,35.6,22.7,39.9C16.2,44.3,8.1,46.4,0,46.4C-8.2,46.5,-16.4,44.6,-23.5,40.6C-30.5,36.6,-36.4,30.5,-40.4,23.4C-44.4,16.3,-46.4,8.1,-46.3,0.1C-46.1,-8,-43.9,-16,-38.9,-21.4C-34,-26.9,-26.4,-29.7,-19.5,-33.4C-12.5,-37.2,-6.3,-41.8,0.6,-42.9C7.5,-44,15.1,-41.6,22.1,-37.9Z"
-              width="100%"
-              height="100%"
-              transform="translate(50 50)"
-              strokeWidth="0"
-              style={{ transition: "all 0.3s ease 0s" }}
-              stroke="url(#sw-gradient)"
-            ></path>
-          </svg>
-         </div>
+          <div>
+            <svg
+              className=""
+              id="sw-js-blob-svg"
+              viewBox="0 0 100 100"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
+                  <stop
+                    id="stop1"
+                    stopColor="rgba(222, 197, 166, 1)"
+                    offset="0%"
+                  ></stop>
+                  <stop
+                    id="stop2"
+                    stopColor="rgba(222, 197, 166, 1)"
+                    offset="100%"
+                  ></stop>
+                </linearGradient>
+              </defs>
+              <path
+                fill="url(#sw-gradient)"
+                d="M22.1,-37.9C29.2,-34.2,35.7,-29.3,39.2,-22.8C42.7,-16.3,43.1,-8.1,42.5,-0.3C41.9,7.5,40.4,14.9,37.2,22C34,29,29.2,35.6,22.7,39.9C16.2,44.3,8.1,46.4,0,46.4C-8.2,46.5,-16.4,44.6,-23.5,40.6C-30.5,36.6,-36.4,30.5,-40.4,23.4C-44.4,16.3,-46.4,8.1,-46.3,0.1C-46.1,-8,-43.9,-16,-38.9,-21.4C-34,-26.9,-26.4,-29.7,-19.5,-33.4C-12.5,-37.2,-6.3,-41.8,0.6,-42.9C7.5,-44,15.1,-41.6,22.1,-37.9Z"
+                width="100%"
+                height="100%"
+                transform="translate(50 50)"
+                strokeWidth="0"
+                style={{ transition: "all 0.3s ease 0s" }}
+                stroke="url(#sw-gradient)"
+              ></path>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
